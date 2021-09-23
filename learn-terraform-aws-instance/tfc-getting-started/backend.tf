673748ea31d4bec2f02c9a4ebe8e52642e7161b1
Alexander Screve <32297178+AlexanderScreve@users.noreply.github.com>
2021-09-23 14:30:34 +0300

    Terraform First Exercise

---
+++ learn-terraform-aws-instance/tfc-getting-started/backend.tf
@@ -0,0 +1,13 @@
+# The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
+# For more information, see https://www.terraform.io/docs/backends/types/remote.html
+terraform {
+  backend "remote" {
+    organization = "example-org-7fc5a9"
+
+    workspaces {
+      name = "getting-started"
+    }
+  }
+
+  required_version = ">= 0.13.0"
+}
